fdd62ff358e798ba09527066317c7abb295d13a0
KRITESH21 <kritesh21march@gmail.com>
2025-10-10 03:38:55 +0530

    Just started and created elements and imported react using cdn

---
+++ App.js
@@ -0,0 +1,28 @@
+//creating an element using React
+
+// const parent = React.createElement("h1", { id: "heading" }, "I am the parent");
+// console.log(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+//creating a nested element child as well as siblings using arrays using React
+
+{/* <div id="root">
+  <div id="parent">
+    <div id="child">
+      <h1>I am an h1 tag</h1>
+      <h2>I am an h2 tag</h2>
+    </div>
+  </div>
+</div> */}
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
