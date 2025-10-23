6fdf05dc186395919a6f6daa77fa6fbad43dd464
KRITESH21 <kritesh21march@gmail.com>
2025-10-23 09:43:55 +0530

    included npm, parcel, package.json and package-lock.json

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 //creating an element using React
 
 // const parent = React.createElement("h1", { id: "heading" }, "I am the parent");
@@ -20,8 +23,8 @@ const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
+    React.createElement("h1", {key:"h1"}, "I am an h1 tag"),
+    React.createElement("h2", {key:"h2"}, "I am an h2 tag"),
   ])
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
